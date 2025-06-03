6fe6c9fc5be793f397c468c3db0d090465942fe2
OtienoKennedy <125959787+OtienoKennedy@users.noreply.github.com>
2025-06-03 17:45:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace with your GitHub repo name
 export default defineConfig({
-  plugins: [react()]
+  base: '/Kennedy_portfolio/',
+  plugins: [react()],
 })
