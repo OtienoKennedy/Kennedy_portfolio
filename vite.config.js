2c8a2bb5e3b9044d450657feeb5f9f2bc203b375
Otieno Kennedy <otienokennedy451@gmail.com>
2025-06-04 12:37:38 +0300

    Fix app and update profile image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace with your GitHub repo name
 export default defineConfig({
   base: '/Kennedy_portfolio/',
   plugins: [react()],
